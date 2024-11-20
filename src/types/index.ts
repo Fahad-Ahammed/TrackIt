f3f92a97f424bb005237ccbd505d0b76d9c4c657
Fahad <fahad04598@gmal.com>
2024-11-20 12:42:33 +0530

    update InputField component and its types

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,10 +16,13 @@ export type FormErrors = {
 export type InputTypes = {
   id: string;
   name: string;
-  placeholder: string;
+  placeholder?: string;
   type?: string;
   value: string;
   error?: string;
   labelBg?: string;
+  autoFocus?: boolean;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
